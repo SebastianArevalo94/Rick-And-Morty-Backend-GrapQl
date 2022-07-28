a0d49c69c7a1a2d4e9ce17ce415e21ed6504694c
SebastianArevalo94 <arevalosebastian081@gmail.com>
2022-07-28 17:55:59 -0500

    backend-complete

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs } from "./GraphQl/typeDefs.js";
 import { resolvers } from "./GraphQl/resolvers.js";
@@ -6,6 +7,7 @@ import "dotenv/config.js";
 import "./API/database.js";
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 5000;
 
 const startServer = async () => {
